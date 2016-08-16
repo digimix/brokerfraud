a6d2499d12fe80c2efcc94d7b6a6f9d75f545584
Digimix <hello@digimix.us>
2016-08-16 18:49:25 -0400

    page templates

---
+++ mix-attorney.php
@@ -0,0 +1,20 @@
+<?php while (have_posts()) : the_post(); ?>
+	<div class="page-content page-columns">
+		<div class="overview">
+			<?php get_template_part('templates/page', 'overview'); ?>
+			<?php //get_template_part('templates/cta-box', 'cases'); ?>
+			<?php //get_template_part('templates/cta-box', 'profiles'); ?>
+			<?php get_template_part('templates/cta-box', 'contact'); ?>
+			<?php get_template_part('templates/cta-box', 'localities'); ?>
+		</div>
+		<div class="main-content">
+			<?php get_template_part('templates/content', 'page'); ?>
+		</div>
+		<div class="sitemap-links">
+			<?php get_template_part('templates/terms', 'claims'); ?>
+		</div>
+	</div>
+<?php endwhile; ?>
+
+<?php get_template_part('templates/story-reviews'); ?>
+<?php get_template_part('templates/story-contact'); ?>
