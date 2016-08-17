ae3aa6465f10734efe393da27d8af901142300fc
Digimix <hello@digimix.us>
2016-08-16 20:37:33 -0400

    contact modal

--- templates/entry-footer.php
+++ templates/entry-footer.php
@@ -4,21 +4,9 @@
 			<h2 class="entry-title">Start recovering your losses</h2>
 			<p>If you or someone you know has lost money investing, you may be eligible to recoup your losses. Fitapelli Kurta accepts all cases on a contingency basis: we only get paid if and when you collect money.</p>
 			<p>Time to file your claim may be limited, so we encourage you to avoid delay. Call 877-238-4175 now to speak to an attorney for free.</p>
-<!--
-			<ul class="list-unstyled">
-				<li>Best in class bioavailability enhancers</li>
-				<li>Advanced multi-phase extended release</li>
-				<li>Maximizing every single muscle potential</li>
-			</ul>
--->
 			<p>
-			<a class="btn btn-1" href="<?= esc_url(home_url('/contact/')); ?>">Contact Us</a>
+				<?php get_template_part('templates/modal-contact', 'btn'); ?>
 			</p>
 		</div>
-<!--
-		<figure class="align-bottom">
-			<img class="img-responsive" src="http://atomic.dev/brokerfraud/atomic-core/img/losses-2.png">
-		</figure>
--->
 	</div>
 </div>
