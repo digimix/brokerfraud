04d94fc1e7c6c1602b757c46337b1159007ad491
Digimix <hello@digimix.us>
2016-08-16 21:21:32 -0400

    crumbs

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php while (have_posts()) : the_post(); ?>
+	<?php get_template_part('templates/breadcrumbs'); ?>
 	<div class="page-content page-columns">
 		<div class="overview">
 			<?php get_template_part('templates/page', 'overview'); ?>
