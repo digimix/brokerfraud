91aa9f041204897a2cff02f66c5330f38ae29a60
Digimix <hello@digimix.us>
2016-08-16 21:12:19 -0400

    crumbs

--- mix-attorney.php
+++ mix-attorney.php
@@ -7,13 +7,14 @@ $slug = get_queried_object()->post_name;
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-	<div class="breadcrumbs-bar">
-		<?php
-		if ( function_exists('yoast_breadcrumb') ) {
-		     yoast_breadcrumb('<p id="breadcrumbs">','</p>');
-		}
-		?>
-	</div>
+
+	<?php
+	if ( function_exists('yoast_breadcrumb') ) {
+	echo '<div class="breadcrumbs-bar">';
+	yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+	echo '</div>';
+	}
+	?>
 	<div class="page-content page-columns">
 		<div class="overview">
 			<?php
