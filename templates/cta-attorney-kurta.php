1325b3b599466473be8509533f2d56625fb8654a
Digimix <hello@digimix.us>
2016-08-16 20:59:58 -0400

    attorney sidebar

--- templates/cta-attorney-kurta.php
+++ templates/cta-attorney-kurta.php
@@ -1,12 +1,14 @@
 <div class="info-attorney-profile jonathan-kurta">
-	<strong class="position">Partner</strong>
-	<a class="emllink" href="mailto:jkurta@fkesq.com?">jkurta@fkesq.com </a>
-	<a class="vclink" href="http://www.stopbrokerfraud.com/Jonathan_2345879.vcf">Download VCard</a>
+	<h4><strong class="position">Partner</strong></h4>
+	<p>
+		<a class="emllink" href="mailto:jkurta@fkesq.com?">jkurta@fkesq.com </a><br/>
+		<a class="vclink" href="http://www.stopbrokerfraud.com/Jonathan_2345879.vcf">Download VCard</a>
+	</p>
 	<p class="blocknumbers">
-		<span id="cno_0_2345885" class="cno_0">Phone: 212.658.1502</span><span id="cno_1_2347908" class="cno_1">Fax: 855.348.2735</span>
+		<span id="cno_0_2345885" class="cno_0">Phone: 212.658.1502</span><br/>
+		<span id="cno_1_2347908" class="cno_1">Fax: 855.348.2735</span>
 	</p>
 	<p class="profileaddress">28 Liberty St, 30th Floor
 	New York, New York 10005</p>
 	<p><a href="https://twitter.com/jonathan_kurta" id="twitter" target="_blank"> <img src="https://lawyers.justia.com/s/twitter-32.png" alt="Twitter"></a> <a href="http://www.linkedin.com/pub/jonathan-kurta/12/243/30b" id="linkedin" target="_blank"> <img src="https://lawyers.justia.com/s/linkedin-32.png" alt="LinkedIn"></a> <a href="https://lawyers.justia.com/lawyer/jonathan-kurta-1327171" id="justiaprofile" target="_blank"> <img src="https://lawyers.justia.com/s/justia-32.png" alt="Justia Profile"></a> <a href="https://plus.google.com/100607629823160052906/about" id="gplus" target="_blank"> <img src="https://lawyers.justia.com/s/google-plus-32.png" alt="Google Plus"></a></p>
 </div>
-
