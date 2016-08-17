27c46977a230ad8917230bfc2e79a7863002ce64
Digimix <hello@digimix.us>
2016-08-17 16:29:41 -0400

    dynamic related cases

--- templates/entry-related-cases.php
+++ templates/entry-related-cases.php
@@ -12,27 +12,5 @@
 			</li>
 		<?php endforeach;
 		wp_reset_postdata();?>
-<!--
-		<li>
-			<div class="copy">
-				<h3>Represented a claimant in connection with an investment in a failed $500 million closed end investment fund that was heavily invested in credit default swaps.</h3>
-			</div>
-			<a href="#">Learn more about: <span>Misrepresentation Claims</span></a>
-		</li>
-		<li>
-			<div class="copy">
-				<h3>Represented small business owner from Texas against mid-sized brokerage firm for allegations of executing unauthorized short positions in his securities account.</h3>
-			</div>
-			<a href="#">Learn more about: <span>Unauthorized Trading Claims</span></a>
-		</li>
-		<li>
-			<div class="copy">
-				<h3>Represented an elderly Ohio retiree against a nationally recognized broker-dealer in connection with unsuitable investments, including the sale of “principal protected” notes.</h3>
-			</div>
-			<a href="#">Learn more about: <span>Breach of Fiduciary Duty Claims</span></a>
-		</li>
--->
 	</ul>
-
-
 </div>
