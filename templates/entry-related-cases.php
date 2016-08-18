cc5318ce65f2f524349dd087ec3f5cb91acedde9
Digimix <hello@digimix.us>
2016-08-17 21:30:45 -0400

    responsive tweaks

--- templates/entry-related-cases.php
+++ templates/entry-related-cases.php
@@ -1,6 +1,8 @@
 <div class="recent-cases">
-	<h2 class="entry-title">Millions of dollars recovered for broker malfeasance</h2>
-	<p>The below is a sampling of recent and representative cases we have handled.</p>
+	<div class="adjust-h-padding">
+		<h2 class="entry-title">Millions of dollars recovered for broker malfeasance</h2>
+		<p>The below is a sampling of recent and representative cases we have handled.</p>
+	</div>
 	<ul class="cases related">
 		<?php
 		$args = array( 'posts_per_page' => 3, 'post_type'=> 'cases', 'orderby'=>'rand' );
